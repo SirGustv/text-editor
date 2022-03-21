e9b6dae476eee2f19b942b7bd4a649dd86972218
Gustavo <gust.bersan@gmail.com>
2022-03-20 23:17:23 -0300

    Criação do método Menu
    Este método orientará o usuário apresentando quais opções ele terá para interagir.

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -6,7 +6,26 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Futuro editor de texto...");
+            Menu();
         }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Editor de texto\n");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo\n2 - Criar novo arquivo\n0 - Sair\n");
+
+            short options = short.Parse(Console.ReadLine());
+
+            switch (options)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: OpenFile(); break;
+                case 2: CreateFile(); break;
+            }
+
+        }
+        static void OpenFile() { }
+        static void CreateFile() { }
     }
 }
